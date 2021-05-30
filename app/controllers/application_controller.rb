ffc4b4c2efa9f28170b9ca13cf6ae8b469a27088
JeremasPosta <60799999+JeremasPosta@users.noreply.github.com>
2021-05-30 19:51:31 -0300

    fix(auth): Mooving before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,2 @@
 class ApplicationController < ActionController::API
-  attr_reader :current_user
-
-  def authenticate_request
-    @current_user = AuthApiRequest.call(request.headers).result
-
-      #tokenite = request.authorization.to_s
-      tokenite = request.headers['Authorization'].split(' ').last.to_s
-
-    puts "\n\n**********************\n\n" + tokenite + "\n\n**********************\n\n"
-    render json: { error: "Not Authorized #{tokenite}" }, status: 401 unless @current_user
-  end
 end
