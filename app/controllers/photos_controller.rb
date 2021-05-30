afac2b55adca35b6ba459381403fac40ccde420f
JeremasPosta <60799999+JeremasPosta@users.noreply.github.com>
2021-05-30 19:59:23 -0300

    fix(auth): Before, ok, but without auth

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,17 @@
 class PhotosController < ApplicationController
-  # before_action :authenticate_request
+   before_action :authenticate_request
   before_action :set_photo, only: [:show, :update, :destroy]
-  # attr_reader :current_user
+  attr_reader :current_user
 
-  # def authenticate_request
-  #   @current_user = AuthApiRequest.call(request.headers).result
+  def authenticate_request
+    @current_user = AuthApiRequest.call(request.headers).result
 
-  #     #tokenite = request.authorization.to_s
-  #     tokenite = request.headers['Authorization'].split(' ').last.to_s
+      #tokenite = request.authorization.to_s
+      tokenite = request.headers['Authorization'].split(' ').last.to_s
 
-  #   puts "\n\n**********************\n\n" + tokenite + "\n\n**********************\n\n"
-  #   render json: { error: "Not Authorized #{tokenite}" }, status: 401 unless @current_user
-  # end
+    puts "\n\n**********************\n\n" + tokenite + "\n\n**********************\n\n"
+    render json: { error: "Not Authorized #{tokenite}" }, status: 401 unless @current_user
+  end
 
 
   # GET /photos
