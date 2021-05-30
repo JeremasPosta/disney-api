aab65ae2b2f9a64d88e5843b65cbf49cc0caf65a
JeremasPosta <60799999+JeremasPosta@users.noreply.github.com>
2021-05-30 20:41:02 -0300

    fix(auth): Bazinga added x2.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,10 +2,12 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
+      puts Rails.application.secrets.secret_key_base.to_s
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
  
     def decode(token)
+      puts Rails.application.secrets.secret_key_base.to_s
       body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
       HashWithIndifferentAccess.new body
     end
