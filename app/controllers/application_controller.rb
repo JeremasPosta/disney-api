fcca3e95f2a4e95ce7999b0da25bc0c5a281b243
JeremasPosta <60799999+JeremasPosta@users.noreply.github.com>
2021-05-30 19:27:34 -0300

    fix(auth):This prints tokenite to debug, again, again.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::API
   def authenticate_request
     @current_user = AuthApiRequest.call(request.headers).result
 
-      tokenite = request.authorization.to_s
+      #tokenite = request.authorization.to_s
+      tokenite = request.headers['Authorization'].split(' ').last.to_s
 
     puts "\n\n**********************\n\n" + tokenite + "\n\n**********************\n\n"
     render json: { error: "Not Authorized #{tokenite}" }, status: 401 unless @current_user
