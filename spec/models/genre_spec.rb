afd15b19ba4a1aa724fcc137c30ab4c1f5ceecd6
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-10-23 21:16:15 -0300

    Feat(auth tests): Tests finished.

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{Genre.new(image: "image.jpg", name: "Sci-Fi")}
+  t_movie = Movie.new(image: "image.jpg", title: "Star Wars", realese: DateTime.now, qualification: 5, genre_id: Genre.last.id)
+  before (:each) {subject.save}
+
+  context "at creation" do
+    it "name should be present" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "name should be at least 1 character" do
+      subject.name = "A"
+      expect(subject).to be_valid
+    end
+
+    it "image should be present" do
+      subject.image = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "image should be at least 2 character" do
+      subject.image = "A"
+      expect(subject).to_not be_valid
+    end
+
+    it "image should not be too short" do
+      subject.image = "Ab"
+      expect(subject).to be_valid
+    end
+
+    it "should have a has_many movies relationship" do
+      t_subject = Genre.reflect_on_association(:movies)
+      expect(t_subject.macro).to eq(:has_many)
+    end
+
+    it "should hold an associated movie" do
+      subject.movies << t_movie
+      expect(subject.movies.first).to have_attributes(title: t_movie.title)
+    end
+
+  end
+
 end
--- spec/requests/auth_api_request_spec.rb
+++ spec/requests/auth_api_request_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "AuthApiRequests", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:valid_sign_up){{"name": "Aquiles Bailo", "email": "aquiles@bailo.com", "password": "123123"}}
+  let(:invalid_sign_up){{"name": "Aquiles Bailo", "email": "aquiles@bailo.com", "password": "123123", "role": "1"}}
+
+  let(:valid_login){{"email": "aquiles@bailo.com", "password": "123123"}}
+  let(:invalid_login){{"email": "aquiles@bailo.com", "password": "123"}}
+
+  describe "POST /sign-up" do
+    it "renders a successfull response" do
+      post sign_up_url, params: valid_sign_up, as: :json
+
+      expect(response).to be_successful
+    end  
   end
+
+  describe "POST /login" do
+    it "renders a successfull response with an 'auth_token'" do
+      post sign_up_url, params: valid_sign_up, as: :json
+      post login_url, params: valid_login, as: :json
+
+      expect(response).to be_successful
+      expect(response.body).to include("auth_token")
+    end
+  end
+
 end
