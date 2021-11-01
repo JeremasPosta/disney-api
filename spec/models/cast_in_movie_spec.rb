be6da3ddb6ca28bc9d508a9bdfc4b9dbc08e9093
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-11-01 00:35:36 -0300

    feat(tests): Some extra tests added.

--- spec/models/cast_in_movie_spec.rb
+++ spec/models/cast_in_movie_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CastInMovie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{CastInMovie.new(character_id: 1, movie_id: 1)}
+  
+  context "at creation" do
+    it "character_id should be present" do
+      subject.character_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "movie_id should be present" do
+      subject.movie_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
 end
